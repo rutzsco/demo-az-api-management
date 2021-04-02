9b582ec3efa29bf3452328e27fd8e696693ca092
Scott Rutz <scrutz@microsoft.com>
2021-04-02 15:06:50 -0500

    Update GenericGetExecutionLogic.cs

--- Services/SimulationService/Simulation.Service/Logic/GenericGetExecutionLogic.cs
+++ Services/SimulationService/Simulation.Service/Logic/GenericGetExecutionLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -11,22 +13,24 @@ namespace Simulation.Service.Logic
         private string _url;
         private string _accessToken;
         private HttpClient _httpClient;
+        private ILogger _logger;
 
-        public GenericGetExecutionLogic(string url, string accessToken, HttpClient httpClient)
+        public GenericGetExecutionLogic(string url, string accessToken, HttpClient httpClient, ILogger logger)
         {
             _url = url ?? throw new ArgumentNullException(nameof(url));
             _accessToken = accessToken ?? throw new ArgumentNullException(nameof(accessToken));
             _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public async void Execute(string api)
         {
-
             if (!string.IsNullOrEmpty(_accessToken))
                 _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _accessToken);
 
             var response = await _httpClient.GetAsync(_url+"/"+ api);
             string result = response.Content.ReadAsStringAsync().Result;
-        }
+            _logger.LogInformation($"API call result - Api:{api} Status: {response.StatusCode}");
+         }
     }
 }
