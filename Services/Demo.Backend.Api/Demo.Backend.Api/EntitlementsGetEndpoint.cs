419088b84bdc474f839f1e9fbf10491102c1145f
Scott Rutz <scrutz@microsoft.com>
2023-03-07 14:26:32 +0000

    cp

---
+++ Services/Demo.Backend.Api/Demo.Backend.Api/EntitlementsGetEndpoint.cs
@@ -0,0 +1,24 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Reflection.PortableExecutable;
+
+namespace Demo.Backend.Api
+{
+    public static class BackendGetEndpoint
+    {
+        [FunctionName("EntitlementsGetEndpoint")]
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req, ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            return new OkObjectResult(new { status = 200 });
+        }
+    }
+}
